8880bc8236c541a8c81d11c7290451b60e2d243d
Jessica Samtani <jessicasamtani@gmail.com>
2023-10-10 23:41:35 +0700

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        veryDarkBlue: "hsl(233, 47%, 7%)",
+        darkSaturatedBlue: "hsl(244, 38%, 16%)",
+        softViolet: "hsl(277, 64%, 61%)",
+        mainParagraph: "hsla(0, 0%, 100%, 0.75)",
+        statHeadings: "hsla(0, 0%, 100%, 0.6)",
+      },
+      fontFamily: {
+        lexend: ["Lexend Deca", "sans-serif"],
+      },
+      backgroundImage: {
+        desktopImage: "url('src/images/image-header-desktop.jpg')",
+        mobileImage: "url('src/images/image-header-mobile.jpg')",
+      },
+    },
   },
   plugins: [],
-}
+};
